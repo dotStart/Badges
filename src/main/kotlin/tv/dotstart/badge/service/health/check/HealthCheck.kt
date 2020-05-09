c8a708ebd281a9e1c32fb60bebcba745e497f4ef
Johannes Donath <johannesd@torchmind.com>
2020-05-09 14:20:45 +0200

    Created a definition for health checks and factories.

---
+++ src/main/kotlin/tv/dotstart/badge/service/health/check/HealthCheck.kt
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2020 Johannes Donath <johannesd@torchmind.com>
+ * and other copyright owners as documented in the project's IP log.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package tv.dotstart.badge.service.health.check
+
+import reactor.core.publisher.Mono
+import tv.dotstart.badge.service.health.Health
+
+/**
+ * Provides a custom health check which evaluates whether any part of the application has degraded
+ * or failed.
+ *
+ * @author [Johannes Donath](mailto:johannesd@torchmind.com)
+ * @date 09/05/2020
+ */
+interface HealthCheck {
+
+  /**
+   * Uniquely identifies this particular check for debugging purposes.
+   */
+  val id: String
+
+  /**
+   * Computes the new health state.
+   */
+  fun getState(): Mono<Health>
+
+  interface Factory {
+    fun create(): List<HealthCheck>
+  }
+}
