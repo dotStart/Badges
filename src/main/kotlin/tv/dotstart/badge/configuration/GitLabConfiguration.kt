559ff888fe44a3765dbf422aae71a4e4ae99ddc2
Johannes Donath <johannesd@torchmind.com>
2020-05-10 00:09:34 +0200

    Imported configuration components for the GitLab connector.

---
+++ src/main/kotlin/tv/dotstart/badge/configuration/GitLabConfiguration.kt
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2020 Johannes Donath <johannesd@torchmind.com>
+ * and other copyright owners as documented in the project's IP log.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package tv.dotstart.badge.configuration
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
+import org.springframework.boot.context.properties.EnableConfigurationProperties
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import tv.dotstart.badge.configuration.properties.GitLabProperties
+import tv.dotstart.badge.service.connector.gitlab.GitLab
+import tv.dotstart.badge.service.counter.ReactiveCounterProvider
+import java.time.Duration
+
+/**
+ * @author [Johannes Donath](mailto:johannesd@torchmind.com)
+ * @date 09/05/2020
+ */
+@Configuration
+@EnableConfigurationProperties(GitLabProperties::class)
+@ConditionalOnProperty("badges.connector.gitlab.enabled", matchIfMissing = true)
+class GitLabConfiguration(private val properties: GitLabProperties) {
+
+  @Bean
+  fun gitLab(counterProvider: ReactiveCounterProvider): GitLab {
+    val counter = counterProvider.create("gitlab_rate_limit",
+                                         Duration.ofSeconds(1))
+
+    return GitLab(this.properties.baseUrl, counter)
+  }
+}
