39ffc3c7d27727b807a7dc750c421c320157b479
Johannes Donath <johannesd@torchmind.com>
2020-05-09 18:31:29 +0200

    Defined the Discord brand color.

---
+++ src/main/kotlin/tv/dotstart/badge/controller/v1/badges/discord/DiscordFunctions.kt
@@ -0,0 +1,31 @@
+/*
+ * Copyright 2020 Johannes Donath <johannesd@torchmind.com>
+ * and other copyright owners as documented in the project's IP log.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package tv.dotstart.badge.controller.v1.badges.discord
+
+import tv.dotstart.badge.util.Color
+
+/**
+ * Provides common functions and definitions for use throughout the Discord badge scope.
+ *
+ * @author [Johannes Donath](mailto:johannesd@torchmind.com)
+ * @date 09/05/2020
+ */
+
+/**
+ * Defines the brand color which is used for badges when no other color variation is preferable.
+ */
+internal val brandColor = Color.BLUE
