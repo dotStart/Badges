68578c30e578dac16524ffab21b4ab110efaeac1
Johannes Donath <johannesd@torchmind.com>
2020-05-09 17:23:59 +0200

    Added support for Discord.

---
+++ src/main/kotlin/tv/dotstart/badge/service/discord/Discord.kt
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2020 Johannes Donath <johannesd@torchmind.com>
+ * and other copyright owners as documented in the project's IP log.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package tv.dotstart.badge.service.discord
+
+import org.springframework.stereotype.Service
+import org.springframework.web.reactive.function.client.WebClient
+import org.springframework.web.reactive.function.client.bodyToMono
+import reactor.core.publisher.Mono
+import tv.dotstart.badge.service.Connector
+import tv.dotstart.badge.service.discord.model.Widget
+
+/**
+ * Provides access to aspects of the Discord API.
+ *
+ * @author [Johannes Donath](mailto:johannesd@torchmind.com)
+ * @date 09/05/2020
+ */
+@Service
+class Discord : Connector {
+
+  private val client = WebClient.builder()
+      .baseUrl("https://discordapp.com/api")
+      .defaultHeaders { headers ->
+        headers["Accept"] = "application/json"
+      }
+      .build()
+
+  override val name = "discord"
+  override val rateLimit = null
+  override fun getRateLimitUsage() = Mono.just(0L)
+
+  /**
+   * Retrieves the contents of the server widget (if enabled).
+   */
+  fun getWidget(serverId: String) = this.client.get()
+      .uri("/guilds/{serverId}/widget.json", serverId)
+      .retrieve()
+      .bodyToMono<Widget>()
+}
